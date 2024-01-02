2364daf9471e4016b118fddc8c28592343a5d845
cdnwell <cdnwellhk@gmail.com>
2024-01-02 22:03:27 +0900

    commit : vue 개인 공부 프로젝트 첫 번째 커밋

---
+++ map-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+var map1;
+window.onload = function(){
+  map1 = new ngii_wmts.map("map1", {zoom : 2});
+};
+</script>
+
+<template>
+  <div id="map1" style="width:100%;height:100%;"></div>
+</template>
+
+<style scoped>
+</style>
